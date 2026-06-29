5377b3efbe1458a479b291ea9a28a8f20ea52b2e
zock06 <dimas.jombang2016@gmail.com>
2026-06-29 07:17:37 +0700

    python project

---
+++ C++ Project/Belajar.cpp
@@ -0,0 +1,9 @@
+# include <iostream>
+
+using namespace std;
+int main() {
+    cout << "Hello world!" << endl;
+    cout << "Yeah!"<<endl << endl << endl;
+    cout << "Dimaz" << endl;
+    return 0;
+}
