85134319a0320f51615208526a4c99ba45bea102
zock06 <dimas.jombang2016@gmail.com>
2026-06-29 07:35:21 +0700

    Hackactive8

--- C++ Project/Belajar.cpp
+++
@@ -1,9 +0,0 @@
-# include <iostream>
-
-using namespace std;
-int main() {
-    cout << "Hello world!" << endl;
-    cout << "Yeah!"<<endl << endl << endl;
-    cout << "Dimaz" << endl;
-    return 0;
-}
